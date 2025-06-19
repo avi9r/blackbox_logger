680da25565b31d35d2ab21919a64bfc325faf851
avi9r <avinashranjan633@gmail.com>
2025-06-20 00:42:16 +0530

    version 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="blackbox_logger",
-    version="0.2.1",
+    version="0.2.2",
     description="Framework-agnostic HTTP logger with payload masking",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
